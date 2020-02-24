bcb69f3e6f587d7a7d898962828bb616ea6081cd
AllenYick <yike347@163.com>
2020-02-24 08:48:26 +0800

    initialize

---
+++ auth.py
@@ -0,0 +1,92 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+# 认证蓝图
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+# 认证蓝图将包括注册新用户、登录和注销视图
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'username is required'
+        elif not password:
+            error = 'password is required'
+        elif db.execute(
+            'SELECT id FROM user WHERE username=?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES(?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        flash(error)
+    return render_template('auth/register.html')
+
+# 注册一个在视图函数之前运行的函数
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        user = db.execute(
+            'SELECT * FROM user where username=?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        flash(error)
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id', None)
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user where id = ?', (user_id,)
+        ).fetchone()
+
+
+@bp.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.clear()    # 把用户 id 从 session 中移除
+    return redirect(url_for('index'))
+
+
+# 在其他视图中验证
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
